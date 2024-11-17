603faeda3ea63daf30a76f9379ad16246b7b91b5
Asmamaw-Abeba <asmeabeba12@gmail.com>
2024-11-17 01:43:14 -0800

    import loadCart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderpaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProductsFetch } from "../data/products.js";
+import {loadCart } from "../data/cart.js";
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
 
